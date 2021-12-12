e460b496571fd6fa4d3e5b2dd7f7a8754102848f
Fernando Maldonado <Fmaldonado824@gmail.com>
2021-12-12 17:00:42 -0600

    Added backwards compatibility on kardex route

--- src/webapi/controllers/kardex/kardexController.ts
+++ src/webapi/controllers/kardex/kardexController.ts
@@ -9,43 +9,74 @@ class KardexController extends BaseController {
         this.router.get("/",
             (req, res, next) => this.verifyToken(req, res, next),
             (req, res) => this.getKardex(req as CustomRequest, res))
+
+        this.router.get("/:id",
+            (req, res, next) => this.verifyToken(req, res, next),
+            (req, res) => this.getKardexByIndex(req as CustomRequest, res))
     }
 
     private async getKardex(req: CustomRequest, res: Response) {
         try {
 
-            const queries = req.query as any
+            const queries = req.query as any as Carrera
 
-            if (!queries.opcion)
-                res.status(400).send("Opcion missing")
+            if (!queries.claveCarrera)
+                res.status(400).send("claveCarrera missing")
 
-            const opcion = queries.opcion as number
+            if (!queries.claveDependencia)
+                res.status(400).send("claveDependencia missing")
 
-            if (opcion >= req.careers.length)
-                return res.status(400).send("Opcion is out of bounds")
+            if (!queries.claveGradoAcademico)
+                res.status(400).send("claveGradoAcademico missing")
 
-            const carrera = req.careers[opcion] as Carrera
+            if (!queries.claveModalidad)
+                res.status(400).send("claveModalidad missing")
 
-            if (!carrera.claveCarrera)
-                res.status(400).send("claveCarrera unavailable")
+            if (!queries.claveNivelAcademico)
+                res.status(400).send("claveNivelAcademico missing")
 
-            if (!carrera.claveDependencia)
-                res.status(400).send("claveDependencia unavailable")
+            if (!queries.clavePlanEstudios)
+                res.status(400).send("clavePlanEstudios missing")
 
-            if (!carrera.claveGradoAcademico)
-                res.status(400).send("claveGradoAcademico unavailable")
+            if (!queries.claveUnidad)
+                res.status(400).send("claveUnidad missing")
 
-            if (!carrera.claveModalidad)
-                res.status(400).send("claveModalidad unavailable")
+            const data = await kardexDataSource.getKardexResponse(queries, req.user, req.trim);
 
-            if (!carrera.claveNivelAcademico)
-                res.status(400).send("claveNivelAcademico unavailable")
+            const kardexScrapper = new KardexScrapper(data)
 
-            if (!carrera.clavePlanEstudios)
-                res.status(400).send("clavePlanEstudios unavailable")
+            const kardex = kardexScrapper.getKardex();
 
-            if (!carrera.claveUnidad)
-                res.status(400).send("claveUnidad unavailable")
+            if (!kardex)
+                return res.sendStatus(404)
+
+            res.status(200).json(kardex)
+
+        } catch (error: any) {
+            console.error(error)
+
+            if (axios.isAxiosError(error))
+                return res.status(503).send("SIASE no funciona")
+
+            res.status(500).send(error.message)
+        }
+    }
+
+    private async getKardexByIndex(req: CustomRequest, res: Response) {
+        try {
+
+            const index = req.params.index
+
+            if (!Number.parseInt(index))
+                return res.status(400).send("Invalid index")
+
+            if (!index)
+                return res.status(400).send("Index missing")
+
+            if (index < 0 || index >= req.careers.length)
+                return res.status(400).send("Index out of bounds")
+
+            const carrera = req.careers[index] as Carrera
 
             const data = await kardexDataSource.getKardexResponse(carrera, req.user, req.trim);
 
